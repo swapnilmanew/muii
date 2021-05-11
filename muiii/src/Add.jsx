c2e1ba1196d33aadd79166fe54bf08020018839a
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-11 15:16:44 +0000

    adf

---
+++ muiii/src/Add.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Add = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                  <div className="col-12 col-md-7 m-auto">
+                  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+                  </div>
+                  <div className="col-12 col-md-5 m-auto">
+
+                  </div>
+                </div>
+        </>
+    )
+}
+
+export default Add
--- muiii/src/App.js
+++ muiii/src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
-
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/" component={Add} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
